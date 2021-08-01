15c7ed1650fdf298d643a370e2926a9c043d677b
yui masuko <yui.t.tom@gmail.com>
2021-08-01 22:28:03 +0900

    rails gで自動生成しない設定

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,14 @@
 require_relative 'boot'
 require 'rails/all'
-
 Bundler.require(*Rails.groups)
-
 module InstaClone
   class Application < Rails::Application
-    config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.load_defaults 5.2
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
