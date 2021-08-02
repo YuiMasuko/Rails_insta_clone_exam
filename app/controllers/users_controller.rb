34db19f9874feb8df90cb839bffc7cd4b8fe2f7a
yui masuko <yui.t.tom@gmail.com>
2021-08-03 08:52:36 +0900

    ユーザー編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = "登録が完了しました！"
       redirect_to user_path(@user.id)
     else
       render :new
@@ -14,6 +15,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "情報を編集しました！"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
